35d4329625435606d70ea82eda39ea5a8762ad72
Luiz Felipe <fffelippe51413121lippe@gmail.com>
2025-04-08 16:00:37 -0300

    comando de SO

--- jogos/adivinhar_numero/lib/inicializacao.rb
+++ jogos/adivinhar_numero/lib/inicializacao.rb
@@ -1,11 +1,13 @@
 class Inicializacao
 
     def self.iniciar
+    system('clear')
     print "Incializando." 
         4.times do |i|
             sleep 1
             print "."
         end
     puts "."
+    system('clear')
     end
 end
